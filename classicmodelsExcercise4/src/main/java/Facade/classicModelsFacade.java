a400aa992220b4f0e361b00e584a05d0644426b2
caspergrosslarsen <caspergrosslarsen@localhost>
2017-08-25 14:02:48 +0200

    Exercise 1 - startup

---
+++ classicmodelsExcercise4/src/main/java/Facade/classicModelsFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Facade;
+
+import Entity.Customers;
+import Entity.Employees;
+
+/**
+ *
+ * @author caspergrosslarsen
+ */
+
+public class classicModelsFacade {
+ 
+    Employees employee = new Employees();
+    Customers customer = new Customers();
+
+}
+    
+//    public Employees createEmployee() {
+//    
+//    }
+//    
+//    public Customers updatecustomer(Customers count) {
+//    
+//    }
+//}
+
